9fa209d8e884d773f33b61a4beb3ee930cfdf5c9
Renee Sims <info@reneesoul.com>
2019-08-18 10:18:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,7 +62,8 @@ class Song
   
   def self.create_from_filename(name)
 binding.pry    
-    song = self.create_by_name("#{name.join(" - ")}")    # @file.name = (name.split(" - ")[1])
+    song = @song.artist_name
+    # @file.name = (name.split(" - ")[1])
     @song = (name.split(" - ")[1].chomp(".mp3"))
     @song.save
     
